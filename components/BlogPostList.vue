fddc4f59da0c230651ff091419e741761756e445
shaharhac <shaharhac@gmail.com>
2022-06-22 22:22:09 +0300

    Feature: creating a blog posts list component

---
+++ components/BlogPostList.vue
@@ -0,0 +1,17 @@
+<script setup>
+const { data: blogPosts } = await useAsyncData("blogPosts", () =>
+  queryContent("/").find()
+);
+</script>
+
+<template>
+  <div>
+    <section class="articles">
+      <div v-for="post in blogPosts" :key="post._path">
+        <a :href="post._path">{{ post.title }}</a>
+      </div>
+    </section>
+  </div>
+</template>
+
+<style></style>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <main>
+    <BlogPostList />
+  </main>
+</template>
