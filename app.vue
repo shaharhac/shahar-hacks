6e2eaae007fc595980ecf4f37bcaca34652a8b28
shaharhac <shaharhac@gmail.com>
2022-12-15 23:08:12 +0200

    setting container to be full height

--- app.vue
+++ app.vue
@@ -3,7 +3,7 @@ const [darkMode] = useDarkMode();
 </script>
 
 <template>
-  <div :class="{ dark: darkMode }" class="w-screen h-screen">
+  <div :class="{ dark: darkMode }" class="w-screen h-max">
     <div class="dark:bg-gray-800 h-full">
       <div
         class="bg-white max-w-sm sm:max-w-lg md:max-w-2xl lg:max-w-4xl mx-auto dark:bg-gray-800 dark:text-white"
