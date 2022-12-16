0971950563f37b95707e8dd3be4cdd8903da5b91
shaharhac <shaharhac@gmail.com>
2022-12-16 10:40:32 +0200

    fixing containers height so dark mode will be presented nicer

--- app.vue
+++ app.vue
@@ -3,10 +3,10 @@ const [darkMode] = useDarkMode();
 </script>
 
 <template>
-  <div :class="{ dark: darkMode }" class="w-screen h-screen">
-    <div class="dark:bg-gray-800 h-full">
+  <div :class="{ dark: darkMode }" class="w-screen">
+    <div class="dark:bg-gray-800 min-h-screen h-max">
       <div
-        class="bg-white max-w-sm sm:max-w-lg md:max-w-2xl lg:max-w-4xl mx-auto dark:bg-gray-800 dark:text-white"
+        class="bg-white max-w-sm sm:max-w-lg md:max-w-2xl lg:max-w-4xl mx-auto h-max dark:bg-gray-800 dark:text-white"
       >
         <Navbar />
         <NuxtPage />
